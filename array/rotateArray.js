933ceac9abd2166fb125a3e29abb5eed2e7c1cbd
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2026-05-18 07:07:27 +0530

    'rotatearray'

--- array/rotateArray.js
+++ array/rotateArray.js
@@ -1,15 +1,14 @@
-const rotateArray=(nums)=>{
-let  left=0;
-let  right=nums.length-1;
-let temp=0
-for(right;left<right;right--){
-    temp=nums[right];
-    nums[right]=nums[left]
-    nums[left]=temp
-    left++
-}
-return nums
-}
+// const rotateArray=(nums,k)=>{
+// for(let i=0;i<k; i++){ //
+//     let last=nums.pop();//
+//    nums.unshift(last)
+    
+// }
+// return nums
+// }
 
-let nums=[9,0,7,8,9,9,6,3,1,5]
-console.log(rotateArray(nums))
+// let nums=[9,0,7,8,9,9,6,3,1,5]
+// let k=3
+// console.log(rotateArray(nums,k))
+
+
