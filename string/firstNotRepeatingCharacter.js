8ee7c3238b934121dd36be2950b30319ed4a0d62
evil-sandeep <chandansandeep2000@gmail.com>
2026-05-23 09:30:06 +0530

    2 or more uniq ch find

--- string/firstNotRepeatingCharacter.js
+++ string/firstNotRepeatingCharacter.js
@@ -1,5 +1,6 @@
 const notRepeatingChar=(str)=>{
 let emptyObj={}
+let result=[]
 for(char of str){
     if(emptyObj[char]){
         emptyObj[char]++
@@ -10,13 +11,18 @@ for(char of str){
 
 for(char of str){
     if(emptyObj[char]===1){
-        return char
+        result.push(char)
+        
+    }
+   
+    if(result.length === 2){
+        return result
     }
 }
-
+ 
 
 }
-let str='madam'
+let str='hello'
 console.log(notRepeatingChar(str))
 
 // /Is problem me hume string ka pehla aisa character find karna hota hai jo sirf ek baar appear hua ho. Sabse pehle hum string ke har 
@@ -27,3 +33,24 @@ console.log(notRepeatingChar(str))
 //  karte hain kyuki hume first non-repeating character original order me chahiye hota hai. Har character ke liye check karte hain ki uska 
 //  count 1 hai ya nahi. Jis character ka count pehli baar 1 milta hai, wahi first non-repeating character hota hai aur usko return kar dete 
 //  hain.
+
+
+//if 2 unique charcter or more need then 
+//Is logic me hum pehle string ke har character ka count object/hashmap me store karte hain. Uske baad hum string ko dobara traverse
+//  karte hain aur check karte hain ki kaunsa character sirf ek baar appear hua hai. Agar kisi character ka count 1 hota hai, toh usko 
+// result array me add kar dete hain kyuki wo unique/non-repeating character hai.
+
+// Hum result array ka size continuously check karte rehte hain. Agar hume first 2 unique characters chahiye, toh jaise hi result array
+// ki length 2 hoti hai, hum result return kar dete hain. Isi tarah agar hume aur zyada unique characters chahiye hote hain, toh hum
+//  required count ke according result array ki length check kar sakte hain.
+// let result=[]
+// for(char of str){
+//     if(emptyObj[char]===1){
+//         result.push(char)
+        
+//     }
+   
+//     if(result.length === 2){
+//         return result
+//     }
+// }
